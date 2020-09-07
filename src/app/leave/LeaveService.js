d42e01dd4c739080ff12a53c9063b5f1b24281c5
vinay singh <vinay.singh@aajtak.com>
2020-09-07 22:33:16 +0530

    save changes

--- src/app/leave/LeaveService.js
+++ src/app/leave/LeaveService.js
@@ -5,7 +5,7 @@ const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 class FaqService {
   
   postLeave(params, payload, token) {
-    return axios.post(`${BACKEND_API_URL}//leaves`, payload, {
+    return axios.post(`${BACKEND_API_URL}/leaves`, payload, {
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${token}`,
